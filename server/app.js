ad3714604a03df650eda2c2b2dc868372706b772
Joanna Chen <x.chenj@gmail.com>
2020-05-07 09:13:20 -0700

    Set up server-side websocket

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
+const { createServer } = require('http');
+const Server = require('socket.io');
 
 require('dotenv').config();
 
 const { port } = require('./config');
 
 const app = express();
+const server = createServer(app);
+const io = Server(server);
+
+server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -15,5 +21,42 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+let numUsers = 0;
 
-app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
+io.on('connect', socket => {
+  let newUser = false;
+
+  socket.on('new message', data => {
+    console.log(`Broadcasting new message: ${data}`);
+    socket.broadcast.emit('new message', {
+      message: data
+    });
+  });
+
+  socket.on('add user', username => {
+    if (newUser) return;
+
+    console.log(`Adding new user: ${username}`);
+    socket.username = username;
+    numUsers += 1;
+    newUser = true;
+
+    socket.broadcast.emit(`${username} joined`, {
+      username: socket.username,
+      numUsers,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    if (newUser) numUsers -= 1;
+
+    socket.broadcast.emit(`${socket.username} left`, {
+      username: socket.username,
+      numUsers,
+    });
+  });
+});
+
+
+
+
