5cbde2a1c8d47e3a51220e86397002ec185e82ff
Joanna Chen <x.chenj@gmail.com>
2020-05-09 13:51:00 -0700

    Add browser incompatibility alert

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,39 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import io from 'socket.io-client';
 import Home from './Home';
 import Welcome from './Welcome';
+import { socketUrl } from '../config';
 
 const App = () => {
   const [username, setUsername] = useState('');
   const [currentUsers, setCurrentUsers] = useState([]);
 
-  const socket = io('https://synewaveshi.herokuapp.com/');
+  const socket = io(socketUrl);
 
   const updateUsername = (name) => {
     setUsername(name);
     socket.emit('add user', name);
   };
 
-  useEffect(() => {
-    socket.on('user joined', data => {
-      // console.log(`${data.username} joined!`);
-      setCurrentUsers(data.currentUsers);
-    })
+  socket.on('user joined', data => {
+    console.log(`${data.username} joined!`);
+    setCurrentUsers(data.currentUsers);
+  })
 
-    socket.on('user left', data => {
-      // console.log('user left!');
-      setCurrentUsers(data.currentUsers);
-    });
+  socket.on('user left', data => {
+    console.log('user left!');
+    setCurrentUsers(data.currentUsers);
+  });
 
-    socket.on('disconnect', () => {
-      // console.log(`user has disconnected.`);
-    });
+  socket.on('disconnect', () => {
+    console.log(`user has disconnected.`);
+  });
 
-    socket.on('reconnect', () => {
-      // console.log(`user has been reconnected!`);
-      if (username) socket.emit('add user', username);
-    });
-  }, [socket, username]);
+  socket.on('reconnect', () => {
+    console.log(`user has been reconnected!`);
+    if (username) socket.emit('add user', username);
+  });
 
   if (!username) return <Home updateUsername={updateUsername} />;
   return <Welcome username={username} currentUsers={currentUsers} socket={socket} />;
