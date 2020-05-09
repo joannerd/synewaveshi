feb1ed371c0b7c7f906572732964a329ed6c32b0
Joanna Chen <x.chenj@gmail.com>
2020-05-08 19:50:35 -0700

    Add socket environment variables

--- config.js
+++ config.js
@@ -2,4 +2,7 @@
 module.exports = {
   environment: process.env.NODE_ENV || 'development',
   port: process.env.PORT || '3001',
+  socketHost: process.env.WDS_SOCKET_HOST || 'localhost',
+  socketPath: process.env.WDS_SOCKET_PATH || '/socket.io',
+  socketUrl: process.env.REACT_APP_SOCKET_URL || 'http://192.168.161.65:3001',
 };
