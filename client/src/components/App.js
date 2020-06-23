ba028898617d9221172664e9a8c070cedc4979f0
Joanna Chen <x.chenj@gmail.com>
2020-06-23 00:46:17 -0700

    Set up prototype chat history

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-import Home from './Home';
+import Splash from './Splash';
 import Welcome from './Welcome';
 import { socketUrl } from '../config';
+import HistoryContext from '../HistoryContext';
+import History from './History';
 
-const App = () => {
-  const [username, setUsername] = useState('');
-  const [currentUsers, setCurrentUsers] = useState([]);
-
-  const socket = io(socketUrl);
-
-  const updateUsername = (name) => {
-    setUsername(name);
-    socket.emit('add user', name);
-  };
-
+const App = ({
+  socket,
+  username,
+  setCurrentUsers,
+  setSyneHistory,
+}) => {
   useEffect(() => {
-    socket.on('user joined', data => {
+    socket.on('user joined', (data) => {
       console.log(`${data.username} joined!`);
       setCurrentUsers(data.currentUsers);
-    })
+      setSyneHistory(data.history);
+    });
 
-    socket.on('user left', data => {
+    socket.on('user left', (data) => {
       console.log('user left!');
       setCurrentUsers(data.currentUsers);
+      setSyneHistory(data.history);
     });
 
     socket.on('disconnect', () => {
@@ -34,10 +33,123 @@ const App = () => {
       console.log(`user has been reconnected!`);
       if (username) socket.emit('add user', username);
     });
-  }, [socket, username])
+  }, [socket, username, setCurrentUsers, setSyneHistory]);
 
-  if (!username) return <Home updateUsername={updateUsername} />;
-  return <Welcome username={username} currentUsers={currentUsers} socket={socket} />;
+  if (!username) return <Splash />;
+  return (
+    <div id="home">
+      <Welcome />
+      <History />
+    </div>
+  );
 };
 
-export default App;
+const AppWithContext = () => {
+  const socket = io(socketUrl);
+
+  const getTimestamp = () => {
+    const currentDate = new Date(Date.now());
+    let H = currentDate.getHours();
+    let MM = currentDate.getMinutes();
+    let period = 'AM';
+
+    if (H > 12) {
+      H = H - 12;
+      period = 'PM';
+    } else if (H === 0) {
+      H = 12;
+    }
+
+    if (MM < 10) MM = `0${MM}`;
+    
+    return `${H}:${MM} ${period}`;
+  };
+
+  const [syneHistory, setSyneHistory] = useState([
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+    {
+      timestamp: getTimestamp(),
+      value: "hello! I'm a demo message. whoooooooooo~",
+      user: 'Joanna',
+    },
+  ]);
+  const [currentUsers, setCurrentUsers] = useState([]);
+  const [username, setUsername] = useState('');
+
+  const updateUsername = (name) => {
+    setUsername(name);
+    socket.emit('add user', name);
+  };
+
+  const state = {
+    socket,
+    username,
+    currentUsers,
+    syneHistory,
+    updateUsername,
+    setCurrentUsers,
+    setSyneHistory,
+  };
+
+  return (
+    <HistoryContext.Provider value={state}>
+      <App
+        socket={socket}
+        username={username}
+        setSyneHistory={setSyneHistory}
+        setCurrentUsers={setCurrentUsers}
+      />
+    </HistoryContext.Provider>
+  );
+}
+
+export default AppWithContext;
