53b2779f1c94d3b604fc5cac876f30b3daadbd4b
Joanna Chen <x.chenj@gmail.com>
2020-06-22 20:05:59 -0700

    Add conditional usersMessage render

--- client/src/components/CurrentUsersList.js
+++ client/src/components/CurrentUsersList.js
@@ -1,22 +1,34 @@
 import React from 'react';
 
-const CurrentUsersList = ({ users }) => (
-  <div className="current-users">
+const CurrentUsersList = ({ users }) => {
+  const usersMessage = users.length > 0 ? (
     <h4>You are currently waving sines with...</h4>
-    {users.map(({ id, username }, i) => {
-      let name = username;
+  ) : (
+    <h4>Invite someone to wave sines with!</h4>
+  );
 
-      if (users.length === 2 && i === 1) {
-        name = ` and ${username}.`;
-      } else if (i !== 0) {
-        i < users.length - 1
-          ? (name = `, ${username}`)
-          : (name = `, and ${username}.`);
-      }
+  const usersList = users.map(({ id, username }, i) => {
+    let name = username;
 
-      return <span key={id}>{name}</span>;
-    })}
-  </div>
-);
+    if (users.length === 2 && i === 1) {
+      name = ` and ${username}.`;
+    } else if (i !== 0) {
+      i < users.length - 1 ? (
+        name = `, ${username}`
+      ) : (
+        name = `, and ${username}.`
+      );
+    }
+
+    return <span key={id}>{name}</span>;
+  });
+
+  return (
+    <div className="current-users">
+      {usersMessage}
+      {usersList}
+    </div>
+  );
+} 
 
 export default CurrentUsersList;
